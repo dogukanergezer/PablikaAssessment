7701ff2b11e9675eaa8fbc756a10f6f13248ec78
dogukanergezer <dogukanergezer@gmail.com>
2022-10-25 23:08:32 +0300

    Add project files.

---
+++ src/Services/ReportService/ReportService.Api/Controllers/ReportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+    }
+}
