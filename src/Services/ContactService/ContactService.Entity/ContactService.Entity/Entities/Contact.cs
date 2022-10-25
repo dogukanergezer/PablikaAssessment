2ea089cd81ae5bbb80e13f070b7fa1314916daf5
dogukanergezer <dogukanergezer@gmail.com>
2022-10-25 23:43:33 +0300

    added contact service entities

---
+++ src/Services/ContactService/ContactService.Entity/ContactService.Entity/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using ContactService.Entity.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactService.Entity.Entities
+{
+    public class Contact:BaseEntity
+    {
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
