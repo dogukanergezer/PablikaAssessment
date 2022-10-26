17714c163655733ef7f7cee874391c243b24e809
dogukanergezer <dogukanergezer@gmail.com>
2022-10-26 22:49:35 +0300

    automapper added for report service

---
+++ src/Services/ReportService/ReportService.Entity/ReportService.Entity/AutoMapper/Profiles/ReportProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ReportService.Entity.DTOs;
+using ReportService.Entity.Entities;
+
+namespace ReportService.Entity.AutoMapper.Profiles
+{
+    public class ReportProfile : Profile
+    {
+        public ReportProfile()
+        {
+            CreateMap<ReportDto, Report>().ReverseMap();
+        }
+    }
+}
