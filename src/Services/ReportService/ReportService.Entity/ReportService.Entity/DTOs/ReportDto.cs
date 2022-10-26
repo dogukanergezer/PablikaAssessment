017086315e9d32a87e54c5197a8fe169a6ccf5be
dogukanergezer <dogukanergezer@gmail.com>
2022-10-26 20:14:30 +0300

    Dtos added for reportService

---
+++ src/Services/ReportService/ReportService.Entity/ReportService.Entity/DTOs/ReportDto.cs
@@ -0,0 +1,16 @@
+﻿using ReportService.Entity.Enums;
+
+namespace ReportService.Entity.DTOs
+{
+    public class ReportDto
+    {
+        public Guid Id { get; set; }
+        public string ReportName { get; set; }
+        public string Location { get; set; }
+        public int PersonCount { get; set; }
+
+        public int PhoneCount { get; set; }
+        public DateTime RequestedDate { get; set; }
+        public ReportEnum status { get; set; }
+    }
+}
---
+++ src/Services/ReportService/ReportService.Entity/ReportService.Entity/DTOs/ReportInfoFromUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ReportService.Entity.DTOs
+{
+    public class ReportInfoFromUserDto
+    {
+        public string Location { get; set; }
+        public int UserCount { get; set; }
+        public int PhoneCount { get; set; }
+    }
+}
