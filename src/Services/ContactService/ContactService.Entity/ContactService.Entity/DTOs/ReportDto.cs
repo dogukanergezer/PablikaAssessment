20620742d46743f6444a00c004e625d61ab68199
dogukanergezer <dogukanergezer@gmail.com>
2022-10-26 20:10:45 +0300

    unitOfWork and Dtos added for contactService

---
+++ src/Services/ContactService/ContactService.Entity/ContactService.Entity/DTOs/ReportDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactService.Entity.DTOs
+{
+    public class ReportDto
+    {
+        public string Location { get; set; }
+        public int UserCount { get; set; }
+        public int PhoneCount { get; set; }
+    }
+}
---
+++ src/Services/ContactService/ContactService.Entity/ContactService.Entity/DTOs/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactService.Entity.DTOs
+{
+    public class UserDto
+    {
+        public UserDto()
+        {
+            ContactDtos = new List<ContactDto>();
+        }
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string CompanyName { get; set; }
+        public ICollection<ContactDto> ContactDtos { get; set; }
+    }
+}
