4b62bb67960ae9f3215b3618ed111f1837e88129
dogukanergezer <dogukanergezer@gmail.com>
2022-10-26 14:19:10 +0300

    dbContext and migrations added for reportService

---
+++ src/Services/ReportService/ReportService.Data/ReportService.Data/Migrations/20221026110906_initial_create_reportServiceDb.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportService.Data.Migrations
+{
+    public partial class initial_create_reportServiceDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "Report",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReportName = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    PersonCount = table.Column<int>(type: "integer", nullable: false),
+                    PhoneCount = table.Column<int>(type: "integer", nullable: false),
+                    RequestedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report",
+                schema: "public");
+        }
+    }
+}
