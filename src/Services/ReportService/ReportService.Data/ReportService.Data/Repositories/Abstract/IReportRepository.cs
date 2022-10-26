1fb55d9114f0f05b61ac20a0076eac1b766c908c
dogukanergezer <dogukanergezer@gmail.com>
2022-10-26 14:24:01 +0300

    repositories added for reportService

---
+++ src/Services/ReportService/ReportService.Data/ReportService.Data/Repositories/Abstract/IReportRepository.cs
@@ -0,0 +1,17 @@
+﻿using ReportService.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportService.Data.Repositories.Abstract
+{
+    public interface IReportRepository
+    {
+        void AddReport(Report report);
+        List<Report> GetReports();
+        Report GetReportById(Guid id);
+        void DeleteReport(Report report);
+    }
+}
---
+++ src/Services/ReportService/ReportService.Data/ReportService.Data/Repositories/Concrete/EntityFramework/ReportRepository.cs
@@ -0,0 +1,39 @@
+﻿using ReportService.Data.Contexts;
+using ReportService.Data.Repositories.Abstract;
+using ReportService.Entity.Entities;
+
+namespace ReportService.Data.Repositories.Concrete.EntityFramework
+{
+    public class ReportRepository: IReportRepository
+    {
+        protected ReportServiceContext _reportContext;
+        public ReportRepository(ReportServiceContext context)
+        {
+            _reportContext = context;
+        }
+
+
+        public void AddReport(Report report)
+        {
+            _reportContext.Reports.Add(report);
+            _reportContext.SaveChanges();
+        }
+
+
+        public List<Report> GetReports()
+        {
+            return _reportContext.Reports.ToList();
+        }
+
+        public Report GetReportById(Guid id)
+        {
+            return _reportContext.Reports.FirstOrDefault(x => x.Id == id);
+        }
+        public void DeleteReport(Report report)
+        {
+            _reportContext.Reports.Remove(report);
+            _reportContext.SaveChanges();
+        }
+
+    }
+}
